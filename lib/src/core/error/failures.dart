e9ca05b951995b883a15e4d3a3dbdbaefc88840a
Yirmerah <jeremy.dormevil@gmail.com>
2021-11-13 14:42:44 +0100

    writing the test and implementing the functionnality of the repository impl

--- lib/src/core/error/failures.dart
+++ lib/src/core/error/failures.dart
@@ -8,11 +8,11 @@ abstract class Failure extends Equatable {
 class ServerFailure extends Failure {
   @override
   // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [];
 }
 
 class CacheFailure extends Failure {
   @override
   // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [];
 }
--- lib/src/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/src/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,10 +1,14 @@
 import 'package:fpdart/src/either.dart';
+import 'package:number_trivia/src/core/error/exceptions.dart';
 import 'package:number_trivia/src/core/error/failures.dart';
 import 'package:number_trivia/src/core/platform/network_info.dart';
 import 'package:number_trivia/src/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/src/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/src/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number_trivia/src/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/src/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+typedef Future<NumberTriviaModel> _ConcreteOrRandomChooser();
 
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   final NumberTriviaRemoteDataSource remoteDataSource;
@@ -18,14 +22,38 @@ class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   });
 
   @override
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
-    // TODO: implement getConcreteNumberTrivia
-    throw UnimplementedError();
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    return await _getTrivia(() {
+      return remoteDataSource.getConcreteNumberTrivia(number);
+    });
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
-    // TODO: implement getRandomNumberTrivia
-    throw UnimplementedError();
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
+    return await _getTrivia(() {
+      return remoteDataSource.getRandomNumberTrivia();
+    });
+  }
+
+  Future<Either<Failure, NumberTrivia>> _getTrivia(
+    _ConcreteOrRandomChooser getConcreteOrRandom,
+  ) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia = await getConcreteOrRandom();
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 }

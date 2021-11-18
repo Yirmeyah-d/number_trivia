539ffdeab3976b2796cd994063740d59ceb39d10
Yirmerah <jeremy.dormevil@gmail.com>
2021-11-18 20:21:02 +0100

    Implementing first part of the test for NumberTriviaBloc using bloc_test package

--- lib/src/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/src/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -2,15 +2,46 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:number_trivia/src/core/utils/input_converter.dart';
 import 'package:number_trivia/src/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/src/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/src/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  }) : super(NumberTriviaInitial()) {
+    on<GetTriviaForConcreteNumber>(_onGetTriviaForConcreteNumber);
+  }
+
+  void _onGetTriviaForConcreteNumber(
+    GetTriviaForConcreteNumber event,
+    Emitter<NumberTriviaState> emit,
+  ) {
+    final inputEither =
+        inputConverter.stringToUnsignedInteger(event.numberString);
+
+    inputEither.fold(
+      (failure) {
+        emit(const NumberTriviaError(message: INVALID_INPUT_FAILURE_MESSAGE));
+      },
+      // Although the "success case" doesn't interest us with the current test,
+      // we still have to handle it somehow.
+      (integer) => throw UnimplementedError(),
+    );
   }
 }
